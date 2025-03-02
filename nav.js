3b0a7a906062def939169bd5520bc1fd3b5f6e02
𝕱𝖚𝖓𝖈𝖙𝖎𝖔𝖓𝖆𝖑 𝖂𝖊𝖇𝖘𝖎𝖙𝖊𝖘 <askcpr@gmail.com>
2025-03-02 14:19:27 -0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,5 +1,5 @@
 // Load the navigation content
-fetch('/nav.html')
+fetch('./nav.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('nav-placeholder').innerHTML = data;
